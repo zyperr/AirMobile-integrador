40dfcc53146915588e6cf760c4a5645b030a9c42
Mati-Giordanengo <mati_giordanengo@hotmail.com>
2026-04-30 17:30:36 -0300

    [ADD] Se creo home y detalle de producto en react utilizando vite js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
