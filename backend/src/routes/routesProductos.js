e7051ceb7753c324f8930563f9da729abaefea8e
zyperr <maxi200vera@gmail.com>
2026-05-10 01:39:34 -0300

    [add] middleware para la subida de imagenes ahora verifica que tipo de imagen y tamaño

--- backend/src/routes/routesProductos.js
+++ backend/src/routes/routesProductos.js
@@ -2,20 +2,20 @@ import { Router } from "express";
 import { verificarToken } from "../middlewares/authMiddleware.js";
 import { obtenerProductos,obtenerProducto,crearProducto, actualizarProducto, bulkUpload,eliminarProducto } from "../controllers/controllerProductos.js";
 import { uploadMiddleware } from "../middlewares/multer.js";
-import multer from "multer";
+import { uploadImg } from "../middlewares/fileFilter.js";
 
 
 const router = Router();
 
 
-const upload = multer({storage:multer.memoryStorage()})
+
 
 
 router.get("/productos",obtenerProductos)
 
 router.get("/:id",obtenerProducto)
 
-router.post("/agregar-producto",[verificarToken,upload.array('imagen_url',3)],crearProducto)
+router.post("/agregar-producto",[verificarToken,uploadImg.array('imagen_url',3)],crearProducto)
 router.put("/actualizar-producto/:id", verificarToken,actualizarProducto)
 
 router.delete("/eliminar-producto/:id", verificarToken,eliminarProducto);
