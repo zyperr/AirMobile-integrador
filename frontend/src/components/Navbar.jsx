f3a2f8fa583f5e84d0adde0a74b9d6532ddb3557
YoelZ2203 <leandro22zamora@gmail.com>
2026-05-12 10:09:22 -0300

    Adaptacion de inicio sesion y iconos de navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,17 +11,23 @@ export default function Navbar({ carrito = [] }) {
                 AirMobile
             </Link>
 
-            <ul className="nav-links">
+            <ul className="nav-links d-flex align-items-center">
                 <li><a href="#" className="active">iPhones</a></li>
                 <li><a href="#">Fundas</a></li>
                 <li><a href="#">Cargadores</a></li>
                 <li><a href="#">Audio</a></li>
             </ul>
             <div className="nav-icons">
-                <span>🔍</span>
-                <span>👤</span>
+                <span>
+                    <i class="bi bi-search"></i>
+                </span>
+                <span>
+                    <i class="bi bi-person"></i>
+                </span>
                 <div className="cart-wrapper">
-                    <span>🛒</span>
+                    <span>
+                        <i class="bi bi-cart"></i>
+                    </span>
                     {carrito.length > 0 && <span className="cart-badge">{carrito.length}</span> || <span className="cart-badge">0</span>}
                 </div>
             </div>
